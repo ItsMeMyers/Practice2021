e7af546bdc2e7c4f2ac35b87775f72d66ef2a194
Gautam-C <gautamawala@gmail.com>
2020-01-23 22:20:26 -0500

    created data recording class

---
+++ src/main/java/frc/robot/subsystems/DataRecorder.java
@@ -0,0 +1,59 @@
+package frc.robot.subsystems;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DataRecorder extends SubsystemBase {
+
+    File data = new File("/home/lvuser/data.csv");
+    PrintWriter pw;
+
+    double speed;
+    double distance;
+    boolean success;
+
+    public DataRecorder() {
+
+        if (!data.exists()) {
+            try {
+                data.createNewFile();
+
+                pw = new PrintWriter(data);
+
+                pw.println("speed,distance,success");
+
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public void setSpeed(double spd) {
+        this.speed = spd;
+    }
+
+    public void setDistance(double dist) {
+        this.distance = dist;
+    }
+
+    public void setSuccess(boolean scs) {
+        this.success = scs;
+    }
+    
+    public void writeData() {
+
+        pw.print("\"");
+        pw.print(speed);
+        pw.print("\", \"");
+        pw.print(distance);
+        pw.print("\", \"");
+        pw.print(success);
+        pw.println("\"");
+    }
+
+}
