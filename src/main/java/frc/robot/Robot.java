b635b1a70beba1a0c777d3e4683b12dcb671ac41
Gautam-C <gautamawala@gmail.com>
2020-01-23 23:54:12 -0500

    removed unused command

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,7 +10,6 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.commands.DriveWithJoysticks;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -23,7 +22,6 @@ public class Robot extends TimedRobot {
 
   private RobotContainer m_robotContainer;
 
-  private Command m_driveWithJoysticks;
 
   /**
    * This function is run when the robot is first started up and should be used for any
