017f5a294d2d8386995e92fb9b33d0a4fc004e0a
Yunfei Zheng <yunfei0311@gmail.com>
2020-02-19 18:43:06 -0500

    Changed RouteFinder so it starts at current pose instead of origin

--- src/main/java/frc/robot/commands/RouteFinder.java
+++ src/main/java/frc/robot/commands/RouteFinder.java
@@ -21,94 +21,92 @@ import frc.robot.subsystems.Drivetrain;
 
 public class RouteFinder extends SubsystemBase {
 
-        private static Drivetrain drivetrain;
+    private static Drivetrain drivetrain;
 
-        public RouteFinder(Drivetrain dt) {
-                RouteFinder.drivetrain = dt;
-        }
+    public RouteFinder(Drivetrain dt) {
+        RouteFinder.drivetrain = dt;
+    }
 
     /**
-     * Creates a command to follow a certain trajectory. TODO: REstructure this so it makes sense
+     * Creates a command to follow a certain trajectory. TODO: REstructure this so
+     * it makes sense
+     * 
      * @param trajectory the trajectory to follow
      * @return the command that satisifes the aforementioned conditions
      */
     public static Command getPathCommand(Trajectory trajectory) {
 
         /**
-        * This declaration is fairly substantial, so we’ll go through it
-        * argument-by-argument:
-        * 
-        * The trajectory: This is the trajectory to be followed; accordingly, we pass
-        * the command the trajectory we just constructed in our earlier steps.
-        * 
-        * The pose supplier: This is a method reference (or lambda) to the drive
-        * subsystem method that returns the pose. The RAMSETE controller needs the
-        * current pose measurement to determine the required wheel outputs.
-        * 
-        * The RAMSETE controller: This is the RamseteController object (Java, C++) that
-        * will perform the path-following computation that translates the current
-        * measured pose and trajectory state into a chassis speed setpoint.
-        * 
-        * The drive feedforward: This is a SimpleMotorFeedforward object (Java, C++)
-        * that will automatically perform the correct feedforward calculation with the
-        * feedforward gains (kS, kV, and kA) that we obtained from the drive
-        * characterization tool.
-        * 
-        * The drive kinematics: This is the DifferentialDriveKinematics object (Java,
-        * C++) that we constructed earlier in our constants file, and will be used to
-        * convert chassis speeds to wheel speeds.
-        * 
-        * The wheel speed supplier: This is a method reference (or lambda) to the drive
-        * subsystem method that returns the wheel speeds
-        * 
-        * The left-side PIDController: This is the PIDController object (Java, C++)
-        * that will track the left-side wheel speed setpoint, using the P gain that we
-        * obtained from the drive characterization tool.
-        * 
-        * The right-side PIDController: This is the PIDController object (Java, C++)
-        * that will track the right-side wheel speed setpoint, using the P gain that we
-        * obtained from the drive characterization tool.
-        * 
-        * The output consumer: This is a method reference (or lambda) to the drive
-        * subsystem method that passes the voltage outputs to the drive motors.
-        * 
-        * The robot drive: This is the drive subsystem itself, included to ensure the
-        * command does not operate on the drive at the same time as any other command
-        * that uses the drive.
-        */
+         * This declaration is fairly substantial, so we’ll go through it
+         * argument-by-argument:
+         * 
+         * The trajectory: This is the trajectory to be followed; accordingly, we pass
+         * the command the trajectory we just constructed in our earlier steps.
+         * 
+         * The pose supplier: This is a method reference (or lambda) to the drive
+         * subsystem method that returns the pose. The RAMSETE controller needs the
+         * current pose measurement to determine the required wheel outputs.
+         * 
+         * The RAMSETE controller: This is the RamseteController object (Java, C++) that
+         * will perform the path-following computation that translates the current
+         * measured pose and trajectory state into a chassis speed setpoint.
+         * 
+         * The drive feedforward: This is a SimpleMotorFeedforward object (Java, C++)
+         * that will automatically perform the correct feedforward calculation with the
+         * feedforward gains (kS, kV, and kA) that we obtained from the drive
+         * characterization tool.
+         * 
+         * The drive kinematics: This is the DifferentialDriveKinematics object (Java,
+         * C++) that we constructed earlier in our constants file, and will be used to
+         * convert chassis speeds to wheel speeds.
+         * 
+         * The wheel speed supplier: This is a method reference (or lambda) to the drive
+         * subsystem method that returns the wheel speeds
+         * 
+         * The left-side PIDController: This is the PIDController object (Java, C++)
+         * that will track the left-side wheel speed setpoint, using the P gain that we
+         * obtained from the drive characterization tool.
+         * 
+         * The right-side PIDController: This is the PIDController object (Java, C++)
+         * that will track the right-side wheel speed setpoint, using the P gain that we
+         * obtained from the drive characterization tool.
+         * 
+         * The output consumer: This is a method reference (or lambda) to the drive
+         * subsystem method that passes the voltage outputs to the drive motors.
+         * 
+         * The robot drive: This is the drive subsystem itself, included to ensure the
+         * command does not operate on the drive at the same time as any other command
+         * that uses the drive.
+         */
         RamseteCommand ramseteCommand = new RamseteCommand(trajectory, // Our desired trajectory
-                drivetrain::getPose,
-                new RamseteController(kRamseteB, kRamseteZeta),
-                new SimpleMotorFeedforward(ksVolts,
-                        kvVoltSecondsPerMeter,
-                        kaVoltSecondsSquaredPerMeter),
-                new DifferentialDriveKinematics(kTrackwidthMeters),
-                drivetrain::getWheelSpeeds,
-                new PIDController(kPDriveVel, 0, 0),
-                new PIDController(kPDriveVel, 0, 0),
+                drivetrain::getPose, new RamseteController(kRamseteB, kRamseteZeta),
+                new SimpleMotorFeedforward(ksVolts, kvVoltSecondsPerMeter, kaVoltSecondsSquaredPerMeter),
+                new DifferentialDriveKinematics(kTrackwidthMeters), drivetrain::getWheelSpeeds,
+                new PIDController(kPDriveVel, 0, 0), new PIDController(kPDriveVel, 0, 0),
                 // RamseteCommand passes volts to the callback
                 drivetrain::tankDriveVolts,
                 // requires the drivetrain, will be interrupted if another command requires it
                 drivetrain);
-        
+
         // Follow the path finding command and then stop
         return ramseteCommand.andThen(() -> drivetrain.tankDriveVolts(0, 0));
     }
 
     /**
-     * This generates a trajectory with a few features:
-     * @param pointx the x-coordinate to end up at
-     * @param pointy the y-coordinate to end up at
+     * This generates a trajectory that starts at the current pose with a few features:
+     * 
+     * @param pointx   the x-coordinate to end up at
+     * @param pointy   the y-coordinate to end up at
      * @param rotation the rotation to end up at
-     * @param list a list of points to go through
+     * @param waypointsList a list of points to go through
      * @return the trajectory that satisfies the aforementioned conditions
      */
-    public static Trajectory trajectorygen(int pointx, int pointy, int rotation, List<Translation2d> list) {
+    public static Trajectory trajectorygen(int pointx, int pointy, int rotation, List<Translation2d> waypointsList) {
         return TrajectoryGenerator.generateTrajectory(
-                // Start at the origin facing the +X direction
-                new Pose2d(0, 0, new Rotation2d(rotation)),
+                // Start at the current pose facing the +X direction
+                drivetrain.getPose(),
                 // Pass through these waypoints
-                list,
+                waypointsList,
                 // End at this location
                 new Pose2d(pointx, pointy, new Rotation2d(rotation)),
                 // Pass config
