70a896f3498746bb67ed635a8e7b2c9a5f7b8d97
kash851 <kashyap.bk.rao@gmail.com>
2020-02-26 18:12:55 -0500

    Created New Files, and the Climber and Climb command

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -7,17 +7,28 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Climber extends SubsystemBase
 {
-    WPI_TalonSRX motor = new WPI_TalonSRX(ClimberConstants.cMotorPort);
-    Solenoid solenoid = new Solenoid(ClimberConstants.cSolenoidPort);
+    WPI_TalonSRX motor;
+    Solenoid solenoid;
+    public Climber()
+    {
+         motor = new WPI_TalonSRX(ClimberConstants.cMotorPort);
+         solenoid = new Solenoid(ClimberConstants.cSolenoidPort);
+    }
+
+    /**Deploy the climbing system */
     public void deploy()
     {
         solenoid.set(true);
     }
-    public void climb()
+    /** Actually climb when the button/trigger is held */
+    public void climb(double value)
     {
+        motor.set(value);
     }
+    /**Stop climbing*/
     public void undeploy()
     {
+        motor.set(0.0);
         solenoid.set(false);
     }
 
